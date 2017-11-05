7b2f071a58d81920c9455900fca77248e2d45f2b
boniihristova <boni16@abv.bg>
2017-11-05 17:18:03 +0200

    Initial commit

---
+++ homework_6/GeometryCalculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose option(triangle,square or radians)");
+            string figureType = Console.ReadLine().ToLower();
+            switch (figureType)
+            {
+                case "triangle":
+                    Console.WriteLine("Enter side");
+                    double b = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter height");
+                    double h = double.Parse(Console.ReadLine());
+                    Console.WriteLine("The are of the triangle is = {0:f2}", PrintTriangleArea(b, h));
+                    break;
+                case "square":
+                    Console.WriteLine("Enter side");
+                    double a = double.Parse(Console.ReadLine());
+                    Console.WriteLine("The are of the square is = {0:f2}", PrintSquareArea(a));
+                    break;
+                case "radians":
+                    Console.WriteLine("Enter  the degrees ypu want to transform in radians");
+                    double r = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Your degrees in radians are = {0:f2}", PrintRadiansArea(r));
+                    break;
+
+
+            }
+        }
+        static double PrintTriangleArea(double b, double h)
+        {
+            return (b * h) / 2;
+        }
+        static double PrintSquareArea(double a)
+        {
+            return a * a;
+        }
+        static double PrintRadiansArea(double r)
+        {
+            return Math.PI * r / 180.0;
+        }
+      
+        
+    }
+}
+      
---
+++ homework_6/GetMax/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number: ");
+            double c = double.Parse(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine("The biggest number is: " + a);
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine("The biggest number is: " + b);
+            }
+            else
+            {
+                Console.WriteLine("The biggest number is: " + c);
+            }
+        }
+    }
+}
